8dda24288b256cc01578244019321108783d03a3
XMG <xmg2004@outlook.com>
2024-11-07 21:20:57 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { useMessageStore } from '@/stores/message';
 import { storeToRefs } from 'pinia';
 import { ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-import { Analytics } from "@vercel/analytics/react"
+
 
 const store = useMessageStore();
 const { message } = storeToRefs(store);
@@ -54,7 +54,7 @@ watch(pageSize, (newSize) => {
 
       </div>
     </header>
-    <Analytics />
+   
     <RouterView />
   </div>
 </template>
